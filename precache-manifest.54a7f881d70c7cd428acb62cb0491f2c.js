caf807217a8a0bc04fe5e7dfa2567f4a6480c31e
defauth98 <neto.daniribeiro@gmail.com>
2021-07-21 16:54:26 -0300

    Updates

---
+++ precache-manifest.54a7f881d70c7cd428acb62cb0491f2c.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cbc2d240523cf5bd2ddd84715e29cd0c",
+    "url": "/trybewallet/index.html"
+  },
+  {
+    "revision": "f0e2f7a16f9942c767b4",
+    "url": "/trybewallet/static/css/main.6f05daed.chunk.css"
+  },
+  {
+    "revision": "38f34b423637487ea552",
+    "url": "/trybewallet/static/js/2.50b2c79f.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/trybewallet/static/js/2.50b2c79f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f0e2f7a16f9942c767b4",
+    "url": "/trybewallet/static/js/main.4a16e1a7.chunk.js"
+  },
+  {
+    "revision": "efdc6867495475ae3922",
+    "url": "/trybewallet/static/js/runtime-main.daf72ee0.js"
+  },
+  {
+    "revision": "9a4d5fd542b2140b088ef8beac109cf6",
+    "url": "/trybewallet/static/media/edit.9a4d5fd5.svg"
+  },
+  {
+    "revision": "a3e4c2f7c32ce9a7ad6fa8d381b9d9cf",
+    "url": "/trybewallet/static/media/trash.a3e4c2f7.svg"
+  }
+]);
